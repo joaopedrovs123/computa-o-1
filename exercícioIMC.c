d882bf323e3fbbf4fc8be73f960e91d93dc1827b
joaopedrovs123 <jsouza.2007@alunos.utfpr.edu.br>
2025-08-13 11:12:16 -0300

    Add files via upload

---
+++ exercícioIMC.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    
+    printf("cauculo de imc \n");
+    printf("altura %f\n", 1.64);
+    printf("peso %F\n", 62.5);
+    
+    printf("imc: %f",62.5/(1.64*1.64) );
+    
+    return 0;
+}
