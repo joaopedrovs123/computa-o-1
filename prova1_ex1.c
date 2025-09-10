75a458d8644617964dcdbcbda5493689e42baba7
joaopedrovs123 <jsouza.2007@alunos.utfpr.edu.br>
2025-09-10 09:09:21 -0300

    Add files via upload

---
+++ prova1_ex1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main() {
+    int quantidade_1, quantidade_2;
+    float valor;
+
+   
+    printf("Digite a quantidade do item 1:\n");
+    scanf("%d", &quantidade_1);
+
+ 
+    printf("Digite a quantidade do item 2:\n");
+    scanf("%d", &quantidade_2); 
+
+
+    #define J 10
+    #define A 1
+
+
+    valor = ((quantidade_1 + quantidade_2) * (J - A)) / 2;
+
+   
+    if (valor > quantidade_1) {
+        printf("O resultado é %.2f e as quantidades são %d e %d\n", valor, quantidade_1, quantidade_2);
+    } else {
+        printf("O resultado é %.2f e as quantidades são %d e %d\n", valor * 0.8, quantidade_1, quantidade_2);
+    }
+
+    return 0;
+}
